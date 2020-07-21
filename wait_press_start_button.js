7ae54090161c5a5735aabc22578269ef8a0ff598
STRNEOH <strneoh@localhost.localdomain>
2020-07-21 16:17:53 -0300

    adding description

--- wait_press_start_button.js
+++ wait_press_start_button.js
@@ -16,15 +16,32 @@ module.exports = function(RED) {
             var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
             var command = {
-                    "type": "processing_modular_V1.0",
-                    "slot": 1,
-                    "compare": {},
-                    "method": "wait_press_start_button"
+                type: "processing_modular_V1_0",
+                slot: 1,
+                compare: {},
+                method: "wait_press_start_button",
+                get_output: {},
             }
             var file = globalContext.get("exportFile")
             var slot = globalContext.get("slot");
-            if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
-            else{file.slots[slot].jig_error.push(command)}
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command);
+                    // file.begin.push(command);
+                }
+                else{
+                    file.slots[3].jig_test.push(command);
+                    // file.end.push(command);
+                }
+            }
             globalContext.set("exportFile", file);
             // node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
             console.log(command)
